a012ec81fbb36c875a71a14f0864b6941344345d
Niels Hoppe <mail@nielshoppe.de>
2024-05-23 15:55:31 +0200

    Add string exercises

---
+++ exercises/strings.py
@@ -0,0 +1,31 @@
+def addition():
+    '''
+    Schreibe eine Funktion, die den Benutzer eine Liste von Zahlen eingeben lässt,
+    diese in eine Liste speichert und anschließend als schriftliche Addition untereinander ausgibt:
+
+            1
+        +  42
+        + 123
+        -----
+        = 166
+        =====
+    '''
+    pass
+
+def input_word_list():
+    '''
+    Übung:
+
+    Schreibe eine Funktion, die den Benutzer eine Liste von Begriffen eingeben lässt (`input()`) und zurück gibt (`return`).
+    Die Begriffe sollen in einer Zeile eingegeben und mit einem Komma getrennt werden.
+    Überschüssige Leerzeichen vor und hinter den Begriffen sollen bereinigt werden.
+    '''
+    pass
+
+def input_ingredients():
+    '''
+    Übung:
+
+    Schreibe eine Funktion, die den Benutzer eine Liste von Zutaten und dazugehörigen Mengen eingeben lässt (`input()`) und als Dictionary zurück gibt.
+    '''
+    pass
