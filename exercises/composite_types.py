e654d4df3cc1d4c35b541b7994b30e292a781a47
MaPankow <162994783+MaPankow@users.noreply.github.com>
2024-05-23 14:22:47 +0200

    erste zwei Aufgaben composite_types

--- exercises/composite_types.py
+++ exercises/composite_types.py
@@ -1,18 +1,34 @@
-SUITS = { 'CLUBS', 'HEARTS', 'SPADES', 'DIAMONDS' }
-RANKS = [ 'ACE', 'KING', 'QUEEN', 'JACK', 'TEN',
-         'NINE', 'EIGHT', 'SEVEN', 'SIX', 'FIVE', 'FOUR', 'THREE', 'TWO' ]
+SUITS = [ 'CLUBS', 'HEARTS', 'SPADES', 'DIAMONDS' ]
+RANKS = [ 'ACE', 'KING', 'QUEEN', 'JACK', 'TEN', 'NINE', 'EIGHT', 'SEVEN', 'SIX', 'FIVE', 'FOUR', 'THREE', 'TWO' ]
 
-def deck() -> list[tuple]:
-    '''Create a deck of cards.
-    Each card is a tuple of suit and rank.
-    '''
-    pass
+def deck():
+    cards = []
+    for suit in SUITS:
+        for rank in RANKS:
+            card = (suit, rank)
+            cards.append(card)
+    return cards
+ 
+print(deck())
+cards = deck()
+print(len(cards))
+
+
+    
 
 def split(deck, pos: int) -> tuple[list, list]:
     ''''Split a deck of cards at a given position.
     Return a tuple containing first and second part of the deck.
     '''
-    pass
+    
+    deck1 = deck[ : pos]
+    deck2 = deck[pos : ]
+    return deck1, deck2
+
+a, b = split(cards, 45)
+print(a)
+print('--------')
+print(b)
 
 def peek(deck: list[tuple]) -> tuple:
     '''Show top card from the deck.
