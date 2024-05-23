caf0d7a966430e7a246dbf12a5ddd2dcc129cd22
MaPankow <162994783+MaPankow@users.noreply.github.com>
2024-05-23 16:46:11 +0200

    Lösung diverser Aufgaben

--- exercises/composite_types.py
+++ exercises/composite_types.py
@@ -1,3 +1,5 @@
+from random import randint, shuffle
+
 SUITS = [ 'CLUBS', 'HEARTS', 'SPADES', 'DIAMONDS' ]
 RANKS = [ 'ACE', 'KING', 'QUEEN', 'JACK', 'TEN', 'NINE', 'EIGHT', 'SEVEN', 'SIX', 'FIVE', 'FOUR', 'THREE', 'TWO' ]
 
@@ -29,18 +31,45 @@ a, b = split(cards, 45)
 print(a)
 print('--------')
 print(b)
-
+print('--------')
 def peek(deck: list[tuple]) -> tuple:
     '''Show top card from the deck.
     '''
-    pass
+    #random.shuffle(deck)
+    return deck[-1]
+    
+print(peek(cards))
+
+print('---------')
 
 def draw(deck: list[tuple], random=False) -> tuple:
     '''Draw a card from the deck.
     Choose a random card when random=True,
     otherwise choose top card.
     '''
-    pass
+    # random = random.shuffle(deck)
+    if random == False:
+        return deck[-1]
+    else:
+        return deck.pop(randint(0, len(deck)-1))
+    
+
+    
+def abfrage():
+    eingabe = input('Möchtest du die Karten mischen? ').strip()
+    if eingabe == 'J':
+        return True
+    elif eingabe == 'N':
+        return False
+    else:
+        print('Du darfst nur mit "J" oder "N" anworten')
+        return abfrage()
+
+mischen = abfrage()
+
+    
+    
+print(draw(cards, mischen))
 
 # filter by suit
 # filter by rank
